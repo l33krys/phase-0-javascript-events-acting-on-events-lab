09177b889a1872aacf419e155e757d8f363c4c56
Krystle Lee <l33.krys@gmail.com>
2023-07-17 12:24:34 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Your code here
+const dodger = document.getElementById("dodger");
+const game = document.getElementById('game');
+// Calculate right limit px; use in place of 360 in moveDodgerRight()
+// const right = game.offsetWidth - dodger.offsetWidth;
+
+
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+
+    if (left > 0) {
+        dodger.style.left = `${left - 1}px`;
+    }
+}
+
+function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+
+    if (left < 360) {
+        dodger.style.left = `${left + 1}px`;
+    }
+}
+
+document.addEventListener("keydown", function(e) {
+    if (e.key === "ArrowLeft") {
+        moveDodgerLeft();
+    } else if (e.key === "ArrowRight") {
+        moveDodgerRight();
+        console.log('moving right');
+    }
+});
+
